80bfceeaedde1e1850e4d44b16e1e0cd4a54c48c
Sheena Bonilla <sheena.myb@gmail.com>
2019-09-03 14:46:46 -0700

    Refactor post route in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,21 +11,22 @@ class EventsController < ApplicationController
 	end
 
 	post '/events' do # create and save new event to database
-		@event = Event.new(:title => params[:event][:title], :location => params[:event][:location], :event_date => params[:event][:event_date], :start_time => params[:event][:start_time], :end_time => params[:event][:end_time], :description => params[:event][:description])
-		existing_event = Event.find_by(:title => @event.title, :event_date => @event.event_date)
-		if existing_event == nil
+		existing_event = Event.find_by(:title => params[:event][:title], :location => params[:event][:location], :event_date => params[:event][:event_date])
+		if Event.all.include?(existing_event) == true
+			#create error display page for duplicate events
+			erb :'events/error'
+		elsif existing_event == nil
+			@event = Event.new(:title => params[:event][:title], :location => params[:event][:location], :event_date => params[:event][:event_date], :start_time => params[:event][:start_time], :end_time => params[:event][:end_time], :description => params[:event][:description])
 			user = User.find_by(:id => session[:id])
 			@event.user_id = user.id
 			@event.save
 			@events = Event.all
 			redirect to "/events/#{@event.id}"
-		elsif Event.all.include?(existing_event)
-			#create error display page for duplicate events
-			erb :'events/error'
 		end
 	end
 
 	get '/my-events' do 
+		# renders page with events created by current user
 		@user = User.find_by(:id => session[:id])
 		@events = Event.all
 		erb :'events/my-events'
